4630e376b821ac979d8fed140ecc36355a60d1b3
Dima <63181651+ZakharovDmitri@users.noreply.github.com>
2020-06-14 21:51:09 +0500

    Add files via upload

---
+++ zadanie7/zadanie7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace zadanie7
+{
+    class Program
+    {
+        public static void ShowAllCombinations<T>(IList<T> arr, string current = "")
+        {
+            if (arr.Count == 0) //если все элементы использованы, выводим на консоль получившуюся строку и возвращаемся
+            {
+                Console.WriteLine(current);
+                return;
+            }
+            for (int i = 0; i < arr.Count; i++) //в цикле для каждого элемента прибавляем его к итоговой строке, создаем новый список из оставшихся элементов, и вызываем эту же функцию рекурсивно с новыми параметрами.
+            {
+                List<T> lst = new List<T>(arr);
+                lst.RemoveAt(i);
+                ShowAllCombinations(lst, current + arr[i].ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            string str = " - ";
+            ShowAllCombinations(arr,str);
+            Console.Read();
+        }
+        
+    }
+}
